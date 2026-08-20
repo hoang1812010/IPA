433f4615ad49c0011fbc33f990f071dc0a8246c2
Ooooo <dinhtienhoang1812010@gmail.com>
2026-08-21 01:49:03 +0700

    Update Bridging Header to include kernel exploit headers
    Added kernel exploit headers and function declaration.

--- IPAApp/IPAApp-Bridging-Header.h
+++ IPAApp/IPAApp-Bridging-Header.h
@@ -1,6 +1,18 @@
 //
-//  Use this file to import your target's public headers that you would like to expose to Swift.
+//  IPAApp-Bridging-Header.h
+//  IPAApp
 //
 
-// Example:
-// #import "exploit.h"
+#ifndef IPAApp_Bridging_Header_h
+#define IPAApp_Bridging_Header_h
+
+// Import kernel exploit headers
+#import "kexploit_opa334.h"
+#import "krw.h"
+#import "kutils.h"
+#import "sandbox_escape.h"
+
+// Khai báo hàm C từ kernel exploit
+int kfp_write_file(const char *src, const char *dst);
+
+#endif
