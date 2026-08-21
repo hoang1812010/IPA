cf83a4f2af65cca0e1490c8909045f4a1a145041
Ooooo <dinhtienhoang1812010@gmail.com>
2026-08-21 20:15:52 +0700

    Update IPAApp-Bridging-Header.h

--- IPAApp/IPAApp-Bridging-Header.h
+++ IPAApp/IPAApp-Bridging-Header.h
@@ -1,18 +1,7 @@
-//
-//  IPAApp-Bridging-Header.h
-//  IPAApp
-//
-
-#ifndef IPAApp_Bridging_Header_h
-#define IPAApp_Bridging_Header_h
-
-// Import kernel exploit headers
 #import "kexploit_opa334.h"
 #import "krw.h"
 #import "kutils.h"
 #import "sandbox_escape.h"
+#import "SandboxEscaper.h"
 
-// Khai báo hàm C từ kernel exploit
 int kfp_write_file(const char *src, const char *dst);
-
-#endif
