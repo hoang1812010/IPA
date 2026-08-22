215c4691dcc44064c50b4c9b2afbd579bc9f13ad
Ooooo <dinhtienhoang1812010@gmail.com>
2026-08-22 13:23:15 +0700

    Refactor bridging header for clarity and updates
    Removed deprecated imports and added comments for clarity.

--- IPAApp/IPAApp-Bridging-Header.h
+++ IPAApp/IPAApp-Bridging-Header.h
@@ -1,7 +1,13 @@
-#import "kexploit_opa334.h"
-#import "krw.h"
-#import "kutils.h"
-#import "sandbox_escape.h"
-#import "SandboxEscaper.h"
+//
+//  IPAApp-Bridging-Header.h
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
 
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+// Kernel exploit functions (iOS 15-16)
 int kfp_write_file(const char *src, const char *dst);
+
+// Sandbox escape functions
+BOOL escapeSandbox(void);
