429d442420c7d116cd0a98b5e2040e4c89fd3488
Ooooo <dinhtienhoang1812010@gmail.com>
2026-08-22 10:57:41 +0700

    Update AppDelegate.swift

--- IPAApp/AppDelegate.swift
+++ IPAApp/AppDelegate.swift
@@ -1,24 +1,35 @@
 import UIKit
 
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        
+        // Kiểm tra xem app đã được kích hoạt trước đó hay chưa
+        let isActivated = UserDefaults.standard.bool(forKey: "App_Activated")
+        
+        if isActivated {
+            // ĐÃ KÍCH HOẠT: Đi thẳng vào màn hình chính (ViewController)
+            let mainVC = ViewController()
+            // Bọc trong UINavigationController để có thanh điều hướng (NavigationBar) nếu cần
+            let navController = UINavigationController(rootViewController: mainVC)
+            window?.rootViewController = navController
+        } else {
+            // CHƯA KÍCH HOẠT: Hiện màn hình nhập Key (LoginViewController)
+            let loginVC = LoginViewController()
+            window?.rootViewController = loginVC
+        }
+        
+        window?.makeKeyAndVisible()
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+    func sceneDidDisconnect(_ scene: UIScene) {}
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+    func sceneWillResignActive(_ scene: UIScene) {}
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+    func sceneDidEnterBackground(_ scene: UIScene) {}
 }
