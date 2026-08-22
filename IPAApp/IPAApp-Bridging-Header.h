984e268f7bcc4f757edda2c1dbd2198772e88e2b
Ooooo <dinhtienhoang1812010@gmail.com>
2026-08-22 19:55:55 +0700

    Update bridging header with new function declarations
    Added kernel exploit and sandbox escape function declarations.

--- IPAApp/IPAApp-Bridging-Header.h
+++ IPAApp/IPAApp-Bridging-Header.h
@@ -1,13 +1,27 @@
 //
 //  IPAApp-Bridging-Header.h
-//  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-// Kernel exploit functions (iOS 15-16)
+#import "mcm_bridge.h"
+#import "vnode.h"
+#import "krw.h"
+#import "kutils.h"
+#import "sandbox_escape.h"
+
+// Kernel exploit functions
+int kexploit_init(void);
 int kfp_write_file(const char *src, const char *dst);
+int32_t kexploit_find_container(const char *bundleID, char *uuidBuffer, int32_t bufferSize);
 
 // Sandbox escape functions
 BOOL escapeSandbox(void);
+int sandbox_escape(uint64_t self_proc);
+int sandbox_elevate_to_root(uint64_t self_proc);
+
+// MCM bridge functions
+NSString *MCMActivateContainerPath(uint64_t cls, NSString *identifier, BOOL group, NSString **error);
+NSString *MCMContainerPathForIdentifier(uint64_t cls, NSString *identifier, BOOL group, NSString **error);
+BOOL MCMBridgeAvailable(void);
