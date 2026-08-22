bf06bff5a1459b4695100dd0972e4eec5d06a612
Ooooo <dinhtienhoang1812010@gmail.com>
2026-08-23 00:07:59 +0700

    Update IPAApp-Bridging-Header.h

--- IPAApp/IPAApp-Bridging-Header.h
+++ IPAApp/IPAApp-Bridging-Header.h
@@ -1,28 +1,16 @@
-//
-//  IPAApp-Bridging-Header.h
-//
-
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import <sys/stat.h>              // ← THÊM DÒNG NÀY cho struct stat
-
-// Import exploit headers
-#import "mcm_bridge.h"
-#import "vnode.h"
-#import "krw.h"
-#import "kutils.h"
-#import "sandbox_escape.h"
+#import <sys/stat.h>
 
 // Kernel exploit functions
-int kexploit_opa334(void);
-uint64_t proc_self(void);
+int kfp_write_file(const char *src, const char *dst);
 
 // Sandbox escape functions
+BOOL escapeSandbox(void);
+uint64_t proc_self(void);
 int sandbox_escape(uint64_t self_proc);
-int sandbox_elevate_to_root(uint64_t self_proc);
 
-// MCM bridge functions (giống 3105)
+// MCM functions (QUAN TRỌNG!)
 NSString *MCMActivateContainerPath(uint64_t cls, NSString *identifier, BOOL group, NSString **error);
 NSString *MCMContainerPathForIdentifier(uint64_t cls, NSString *identifier, BOOL group, NSString **error);
 BOOL MCMBridgeAvailable(void);
-NSArray<NSString *> *MCMEnumerateIdentifiersForClass(uint64_t cls, NSUInteger limit, NSString **error);
