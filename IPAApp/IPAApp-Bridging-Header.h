c84487a62e82dd366575ae380c2ecb211bc132ad
Ooooo <dinhtienhoang1812010@gmail.com>
2026-08-23 00:13:41 +0700

    Update IPAApp-Bridging-Header.h

--- IPAApp/IPAApp-Bridging-Header.h
+++ IPAApp/IPAApp-Bridging-Header.h
@@ -1,16 +1,32 @@
+//
+//  IPAApp-Bridging-Header.h
+//
+
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <sys/stat.h>
 
+// Import exploit headers
+#import "mcm_bridge.h"
+#import "vnode.h"
+#import "krw.h"
+#import "kutils.h"
+#import "sandbox_escape.h"
+
 // Kernel exploit functions
-int kfp_write_file(const char *src, const char *dst);
+int kexploit_opa334(void);
+uint64_t proc_self(void);
 
 // Sandbox escape functions
 BOOL escapeSandbox(void);
-uint64_t proc_self(void);
 int sandbox_escape(uint64_t self_proc);
+int sandbox_elevate_to_root(uint64_t self_proc);
 
-// MCM functions (QUAN TRỌNG!)
+// MCM bridge functions (QUAN TRỌNG - cần cho DownloadManager)
 NSString *MCMActivateContainerPath(uint64_t cls, NSString *identifier, BOOL group, NSString **error);
 NSString *MCMContainerPathForIdentifier(uint64_t cls, NSString *identifier, BOOL group, NSString **error);
 BOOL MCMBridgeAvailable(void);
+NSArray<NSString *> *MCMEnumerateIdentifiersForClass(uint64_t cls, NSUInteger limit, NSString **error);
+
+// Kernel file write (iOS 15-16 fallback)
+int kfp_write_file(const char *src, const char *dst);
