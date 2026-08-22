5b522a95eedc51b0ccd62add7380c93b1112d033
Ooooo <dinhtienhoang1812010@gmail.com>
2026-08-22 14:17:10 +0700

    Update MHAController.m

--- IPAApp/MHAController.m
+++ IPAApp/MHAController.m
@@ -2,6 +2,24 @@
 
 @implementation MHAController
 
++ (instancetype)shared {
+    static MHAController *instance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[MHAController alloc] init];
+    });
+    return instance;
+}
+
+- (NSString *)mountContainerForBundleID:(NSString *)bundleID error:(NSError **)error {
+    NSLog(@"[MHA] Mounting container for bundle ID: %@", bundleID);
+    
+    // Với MHA-C2, khi có đúng entitlements, FileManager có thể truy cập trực tiếp
+    // Trả về đường dẫn container tiêu chuẩn của iOS
+    NSString *containerPath = [NSString stringWithFormat:@"/private/var/mobile/Containers/Data/Application/%@", bundleID];
+    return containerPath;
+}
+
 - (BOOL)applyPatchFromFile:(NSString *)patchPath toContainerPath:(NSString *)containerPath error:(NSError **)error {
     NSLog(@"[MHA] Applying patch from %@ to %@", patchPath, containerPath);
     
@@ -19,8 +37,6 @@
     }
     
     // 3. Copy file mod vào container của Free Fire
-    // (Nhờ Entitlements com.apple.private.security.storage.AppDataContainers, 
-    // FileManager có quyền ghi vào container của app khác)
     BOOL success = [fm copyItemAtPath:patchPath toPath:containerPath error:error];
     
     if (success) {
