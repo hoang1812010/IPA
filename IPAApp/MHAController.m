ab822278f3d1a5976e6a3273a866b83e6b7483ed
Ooooo <dinhtienhoang1812010@gmail.com>
2026-08-22 14:03:52 +0700

    Update MHAController.m

--- IPAApp/MHAController.m
+++ IPAApp/MHAController.m
@@ -1,33 +1,35 @@
-// MHAController.m - Implement logic MHA-C2
 #import "MHAController.h"
-#import <dlfcn.h>
 
 @implementation MHAController
 
-+ (instancetype)shared {
-    static MHAController *instance = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{ instance = [[self alloc] init]; });
-    return instance;
-}
-
-- (NSString *)mountContainerForBundleID:(NSString *)bundleID error:(NSError **)error {
-    // Logic kết nối tới housearrest service và gửi lệnh VendContainer
-    // Tham khảo PoC của 0xjohnny: https://github.com/0xjohnny/MobileHouseArrest-PoC
+- (BOOL)applyPatchFromFile:(NSString *)patchPath toContainerPath:(NSString *)containerPath error:(NSError **)error {
+    NSLog(@"[MHA] Applying patch from %@ to %@", patchPath, containerPath);
     
-    NSURL *appURL = [self containerURLForBundleID:bundleID];
-    if (!appURL) {
-        *error = [NSError errorWithDomain:@"MHA" code:404 
-                                 userInfo:@{NSLocalizedDescriptionKey: @"Không tìm thấy container"}];
-        return nil;
+    NSFileManager *fm = [NSFileManager defaultManager];
+    
+    // 1. Tạo thư mục đích nếu chưa có
+    NSString *destDir = [containerPath stringByDeletingLastPathComponent];
+    if (![fm fileExistsAtPath:destDir]) {
+        [fm createDirectoryAtPath:destDir withIntermediateDirectories:YES attributes:nil error:nil];
     }
-    return [appURL path];
-}
-
-- (NSURL *)containerURLForBundleID:(NSString *)bundleID {
-    // Dùng LSApplicationWorkspace để resolve container path
-    // (Cần dlopen MobileCoreServices)
-    return nil; // TODO: implement
+    
+    // 2. Xóa file cũ nếu có
+    if ([fm fileExistsAtPath:containerPath]) {
+        [fm removeItemAtPath:containerPath error:nil];
+    }
+    
+    // 3. Copy file mod vào container của Free Fire
+    // (Nhờ Entitlements com.apple.private.security.storage.AppDataContainers, 
+    // FileManager có quyền ghi vào container của app khác)
+    BOOL success = [fm copyItemAtPath:patchPath toPath:containerPath error:error];
+    
+    if (success) {
+        NSLog(@"[MHA] ✅ Inject successful!");
+    } else {
+        NSLog(@"[MHA] ❌ Inject failed: %@", *error);
+    }
+    
+    return success;
 }
 
 @end
